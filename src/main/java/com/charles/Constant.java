be1f6f643bb695cadd343a1b6cc172716e38402f
Clarence <limao693@sina.com>
2017-08-14 23:47:05 +0800

    Add Constant INFO

--- src/main/java/com/charles/Constant.java
+++ src/main/java/com/charles/Constant.java
@@ -6,5 +6,9 @@ import java.util.Locale;
  * Created by Administrator on 2017/7/14.
  */
 public class Constant {
-    public static final Locale LOCALE = Locale.getDefault();
+    public static final String PROG_NAME = "ShadowSocks-test";
+    public static final String VERSION = "0.1";
+    public static final int BUFFER_SIZE = 16384;
+    public static final String CONF_FILE = "config.json";
+    public static final Locale LOCALE = Locale.getDefault();    //默认区域信息，zh_CN
 }

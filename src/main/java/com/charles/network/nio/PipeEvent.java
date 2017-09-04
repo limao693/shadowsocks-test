af8683ff437bef1448553f07bee111e27bee4b22
Clarence <limao693@sina.com>
2017-09-04 21:37:30 +0800

    add PipwEvent

--- src/main/java/com/charles/network/nio/PipeEvent.java
+++ src/main/java/com/charles/network/nio/PipeEvent.java
@@ -2,11 +2,12 @@ package com.charles.network.nio;
 
 /**
  * Created by Administrator on 2017/9/4.
+ * pipe event for pipe worker
  */
 public class PipeEvent {
     public byte[] data;
     public boolean isEncrypted;
-    
+
     public PipeEvent() {}
 
     public PipeEvent(byte[] data, boolean isEncrypted) {

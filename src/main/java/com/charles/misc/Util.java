b482883390f234b8bf77a068e4777abf58888a67
Clarence <limao693@sina.com>
2017-09-04 16:19:08 +0800

    add saveFile

--- src/main/java/com/charles/misc/Util.java
+++ src/main/java/com/charles/misc/Util.java
@@ -83,6 +83,56 @@ public class Util {
         return retValue;
     }
 
+    public static String getRequestedHostInfo(byte[] data) {
+        String ret = " ";
+        int port;
+        int neededLength;
+        switch (data[0]) {
+            case Socks5Proxy.ATYP_IP_V4:
+                //IPV4
+                neededLength = 6;
+                if (data.length > neededLength) {
+                    port = getPort(data[5], data[6]);
+                    ret = String.format("%d.&d.%d.%d:%d", data[1], data[2], data[3], data[4], port);
+                }
+                break;
+            case Socks5Proxy.ATYP_DOMAIN_NAME:
+                //domain
+                neededLength = data[1];
+                if (data.length > neededLength + 3) {
+                    port = getPort(data[neededLength + 2], data[neededLength + 3]);
+                    String domain = bytesToString(data, 2, neededLength);
+                    ret = String.format("%s:%d", domain, port);
+                }
+                break;
+            case Socks5Proxy.ATYP_IP_V6:
+                //IPV6
+                //16 bytes of IP, 2 bytes of port
+                neededLength = 18;
+                if (data.length > neededLength) {
+                    port = getPort(data[17], data[18]);
+                    ret = String.format("%x%x:%x%x:%x%x:%x%x:%x%x:%x%x:%x%x:%x%x:%d",
+                            data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8],
+                            data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16],
+                            port);
+                }
+                break;
+        }
+        return ret;
+    }
+
+    public static boolean saveFile(String fn, String content) {
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter(fn);
+            writer.println(content);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            return false;
+        }
+        return true;
+    }
+
     public static byte[] composeSSHeader(String host, int port) {
         //TYPE (1 byte) + LENGTH (1 byte) + HOST(var byte) + PORT(2 bytes)
         byte[] respData = new byte[host.length() + 4];
@@ -92,7 +142,6 @@ public class Util {
         System.arraycopy(host.getBytes(), 0, respData, 2, host.length());
         respData[host.length() + 2] = (byte) (port >> 8);
         respData[host.length() + 3] = (byte) (port & 0xFF);
-
         return respData;
     }
 
@@ -104,5 +153,12 @@ public class Util {
             e.printStackTrace();    //print at command line
         }
         return str;
+    }
+
+    private static short bytesToUnsignedByte(byte b) {
+        return (short) (b & 0xff);
+    }
+    private static int getPort(byte b, byte b1) {
+        return bytesToUnsignedByte(b) << 8 | bytesToUnsignedByte(b1);
     }
 }

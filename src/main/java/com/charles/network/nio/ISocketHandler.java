2ff1079238e6f10cb77f3313c3c3ac561d5cece8
Clarence <limao693@sina.com>
2017-09-04 23:18:33 +0800

    change as interface

--- src/main/java/com/charles/network/nio/ISocketHandler.java
+++ src/main/java/com/charles/network/nio/ISocketHandler.java
@@ -1,9 +1,12 @@
 package com.charles.network.nio;
 
+
+
 /**
  * Created by Administrator on 2017/9/4.
+ * Interface of socket hander;
  */
-public class ISocketHandler {
+public interface ISocketHandler {
     void send(ChangeRequest request, byte[] data);
     void send(ChangeRequest request);
 }
